52c68fb6e9b8e227cfabb171118560c3c4d7ca61
ShageeshanT <185689517+ShageeshanT@users.noreply.github.com>
2026-06-17 09:57:00 +0000

    feat: add exportable public API

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export { DEFAULT_PRESETS, getPreset } from './presets.js';
+export { normalizeOffsets, isValidOffset } from './validation.js';
+export { dueAlerts, createAlertMessage } from './scheduler.js';
+export { evaluateSession } from './engine.js';
+export { createRouteSnapshot, isRouteActive } from './routeSnapshot.js';
+export { DEFAULT_SETTINGS, mergeSettings } from './settings.js';

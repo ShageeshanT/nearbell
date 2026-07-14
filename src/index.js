d3e683969d85d24c2c75b53a78dd42096c178a2a
ShageeshanT <185689517+ShageeshanT@users.noreply.github.com>
2026-07-14 19:53:10 +0000

    export: expose mapsPermissionHint helper

--- src/index.js
+++ src/index.js
@@ -132,3 +132,4 @@ export * from './routeRefreshHint.js';
 export * from './notificationRetryLabel.js';
 export * from './testerResultLabel.js';
 // alertWindowLabel export verified for 2026-07-14
+export * from './mapsPermissionHint.js';

ac95ee9dbadaab695414f9bd2a960790c2fa51de
Shagee <it24103322@my.sliit.lk>
2026-08-06 14:33:04 +0530

    geofence-toolkit: add src/index.js barrel export
    Single entry point re-exporting every public function so consumers can
    `require('geofence-toolkit')` instead of reaching into src/*.

---
+++ geofence-toolkit/src/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const { haversineDistance, EARTH_RADIUS_METERS } = require('./haversine');
+const units = require('./units');
+const { isInsideGeofence, distanceToGeofenceEdge } = require('./geofence');
+const { estimateEtaSeconds, kmhToMs, formatEta } = require('./eta');
+const { shouldTriggerAlert } = require('./alertTrigger');
+const { loadGeofencesFromFile, loadGeofencesFromString } = require('./configLoader');
+const { validateGeofence } = require('./validate');
+const { createLogger } = require('./logger');
+
+module.exports = {
+  haversineDistance,
+  EARTH_RADIUS_METERS,
+  ...units,
+  isInsideGeofence,
+  distanceToGeofenceEdge,
+  estimateEtaSeconds,
+  kmhToMs,
+  formatEta,
+  shouldTriggerAlert,
+  loadGeofencesFromFile,
+  loadGeofencesFromString,
+  validateGeofence,
+  createLogger,
+};
